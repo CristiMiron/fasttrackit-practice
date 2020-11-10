9cc1788ab83136ff65466ff82f5b5a854867a020
cristiandan.miron@gmail.com <cristiandan.miron@gmail.com>
2020-11-10 14:57:34 +0200

    Homework Generics

--- src/main/java/ro/fastrackit/classroom/GenericDemo.java
+++ src/main/java/ro/fastrackit/classroom/GenericDemo.java
@@ -1,23 +1,25 @@
 package ro.fastrackit.classroom;
 
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class GenericDemo {
 
     public static void main(String[] args) {
+       ArrayList<? extends Human> humanList = createStudentList();
+       Human a = humanList.get(0);
+        System.out.println(humanList);
+
     }
 
-public StudentType createStudentList () {
-        return createStudentList();
-}
-
-
-
-
-
-
-
+    static ArrayList<Student> createStudentList (){
+        ArrayList<Student> students = new ArrayList<>();
+    Student cristi = new Student("Cristi", "Miron");
+    Student nicolae = new Student("Nicolae", "Guta");
+    Student calin = new Student("Calin", "Goia");
+    students.add(cristi);
+    students.add(nicolae);
+    students.add(calin);
+    return students;
+    }
 
 }
--- src/main/java/ro/fastrackit/classroom/StudentType.java
+++ src/main/java/ro/fastrackit/classroom/StudentType.java
@@ -1,8 +1,12 @@
 package ro.fastrackit.classroom;
 
 public enum StudentType implements ScholarType{
-    BACHELOR,
-    MASTERS,
-    PHD;
+    BACHELOR(0),
+    MASTERS (1),
+    PHD (2);
 
+    public int indexInt;
+    StudentType(int index){
+        this.indexInt = index;
+    }
 }
