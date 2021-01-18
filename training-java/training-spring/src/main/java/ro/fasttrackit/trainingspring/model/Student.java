104f92ed0118c8cd023d57be30b6e7191aaa0851
crist <cristiandan.miron@gmail.com>
2021-01-18 16:51:56 +0200

    aplicatie

---
+++ training-java/training-spring/src/main/java/ro/fasttrackit/trainingspring/model/Student.java
@@ -0,0 +1,42 @@
+package ro.fasttrackit.trainingspring.model;
+
+import java.util.Date;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
+}
